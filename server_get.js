7929ea9fee455f5d27fd4a45f6d016c757dd80f8
DennisYu <yupeif@deakin.edu.au>
2024-03-22 15:47:09 +1100

    Update2

--- server_get.js
+++ server_get.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
 const res = require('express/lib/response');
-const addTwoNumbers = (n1,n2) => {
-    return n1+n2;
+
+const addTwoNumbers = (n1, n2) => {
+    return n1 + n2;
 }
 
-app.get("/addTwoNumbers",(req,res)=>{
+app.get("/addTwoNumbers", (req, res) => {
     const n1 = parseInt(req.query.n1);
     const n2 = parseInt(req.query.n2);
-    const result = addTwoNumbers(n1,n2);
-    res.json({statuscode:200,result:result});
-
+    const result = addTwoNumbers(n1, n2);
+    res.json({
+        statuscode: 200,
+        result: result
+    });
 })
 
-console.log(addTwoNumbers(19,23));
-const port =3000;
+console.log(addTwoNumbers(19, 23));
 
-app.listen(port,()=>{
-    console.log('Server is running on port '+port);
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 })
